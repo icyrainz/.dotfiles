d706f723c8acdccc41975ad85946137bce976ec5
Tue Phan <icyrainz@gmail.com>
2023-05-19 13:25:41 -0700

    Update floaterm

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -1,18 +1,22 @@
 return {
-  "voldikss/vim-floaterm",
-  config = function()
-    vim.keymap.set('n', "<leader>t", ":FloatermToggle<CR>", { desc = "Toggle terminal" })
-    vim.keymap.set('n', "<F5>", ":FloatermToggle<CR>", { desc = "Toggle terminal" })
-    vim.keymap.set('t', "<F5>", "<C-\\><C-n>:FloatermToggle<CR>", { desc = " Toggle terminal" })
-    vim.keymap.set('n', "<F6>", ":FloatermNew<CR>", { desc = "New terminal" })
-    vim.keymap.set('t', "<F6>", "<C-\\><C-n>:FloatermNew<CR>", { desc = "New terminal" })
-    vim.keymap.set('n', "<F7>", ":FloatermKill!<CR>", { desc = "Kill terminal" })
-    vim.keymap.set('t', "<F7>", "<C-\\><C-n>:FloatermKill!<CR>", { desc = "Kill terminal" })
-    vim.keymap.set('n', "<a-n>", ":FloatermNext<CR>", { desc = "Next terminal" })
-    vim.keymap.set('t', "<a-n>", "<C-\\><C-n>:FloatermNext<CR>", { desc = "Next terminal" })
-    vim.keymap.set('n', "<a-p>", ":FloatermPrev<CR>", { desc = "Previous terminal" })
-    vim.keymap.set('t', "<a-p>", "<C-\\><C-n>:FloatermPrev<CR>", { desc = "Previous terminal" })
+	"voldikss/vim-floaterm",
+	config = function()
+		local function opts(desc)
+			return { silent = true, desc = "toggleterm: " .. desc }
+		end
 
-    vim.keymap.set('t', "<Esc>", "<C-\\><C-n>", { desc = "Switch from terminal to normal mode" })
-  end,
+		vim.keymap.set("n", "<leader>t", ":FloatermToggle<CR>", opts("Toggle terminal") )
+		vim.keymap.set("n", "<F5>", ":FloatermToggle<CR>", opts("Toggle terminal") )
+		vim.keymap.set("t", "<F5>", "<C-\\><C-n>:FloatermToggle<CR>", opts(" Toggle terminal") )
+		vim.keymap.set("n", "<F6>", ":FloatermNew<CR>", opts("New terminal") )
+		vim.keymap.set("t", "<F6>", "<C-\\><C-n>:FloatermNew<CR>", opts("New terminal") )
+		vim.keymap.set("n", "<F7>", ":FloatermKill!<CR>", opts("Kill terminal") )
+		vim.keymap.set("t", "<F7>", "<C-\\><C-n>:FloatermKill!<CR>", opts("Kill terminal") )
+		vim.keymap.set("n", "<a-n>", ":FloatermNext<CR>", opts("Next terminal") )
+		vim.keymap.set("t", "<a-n>", "<C-\\><C-n>:FloatermNext<CR>", opts("Next terminal") )
+		vim.keymap.set("n", "<a-p>", ":FloatermPrev<CR>", opts("Previous terminal") )
+		vim.keymap.set("t", "<a-p>", "<C-\\><C-n>:FloatermPrev<CR>", opts("Previous terminal") )
+
+		vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts("Switch from terminal to normal mode") )
+	end,
 }
