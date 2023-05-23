d5d437591467bebce7715388f045a3b8d31e32d2
Tue Phan <icyrainz@gmail.com>
2023-05-23 09:19:38 -0700

    Split tools

---
+++ install_tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+programs=(exa fd bat fzf dash htop lazygit ripgrep thefuck tree eslint nvim direnv)
+fonts=(font-code-new-roman-nerd-font font-hack-nerd-font font-agave-nerd-font font-jetbrains-mono-nerd-font)
+
+if [ -x "$(command -v brew)" ]; then
+    brew install ${programs[@]}
+    brew install ${fonts[@]}
+elif [ -x "$(command -v pacman)" ]; then
+    sudo pacman -S ${programs[@]}
+    sudo pacman -S ${fonts[@]}
+fi
