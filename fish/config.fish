801695f3b153b4b43053ae84c025eec19839de86
Tue Phan <icyrainz@gmail.com>
2023-05-26 22:03:28 -0700

    add fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    cd $HOME
+end
+
+abbr v nvim
+abbr lg lazygit
+# abbr z zoxide
+
+set PATH /opt/homebrew/bin $PATH
+set PATH /opt/homebrew/sbin $PATH
--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-programs=(exa lsd fd bat dust procs tealdeer bottom broot skim tokei sd bandwhich fzf dash htop lazygit ripgrep thefuck nvim direnv)
+programs=(exa lsd fd bat dust procs tealdeer bottom broot skim tokei sd bandwhich fzf dash htop lazygit ripgrep thefuck nvim direnv fish mprocs)
 fonts=(font-hack-nerd-font font-agave-nerd-font font-jetbrains-mono-nerd-font)
 
 if [ -x "$(command -v brew)" ]; then
