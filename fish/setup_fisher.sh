10efa43a4468e48cc3675ec66ca11f0ccc620902
Tue Akio <icyrainz@gmail.com>
2023-05-29 15:40:30 -0700

    Update fisher script

---
+++ fish/setup_fisher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if Fisher is installed
+if ! fish -c 'type -q fisher' >/dev/null
+then
+    # Install Fisher
+    fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
+fi
+
+# List of plugins to install
+plugins=("jorgebucaran/fisher" "jethrokuan/z" "budimanjojo/tmux.fish" "jethrokuan/fzf" "ilancosman/tide@v5" "jorgebucaran/nvm.fish")
+
+# Install plugins
+for plugin in "${plugins[@]}"
+do
+    fish -c "fisher install $plugin"
+done
--- install_tools.sh
+++ install_tools.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-programs=(exa lsd fd bat dust procs tealdeer bottom broot skim tokei sd bandwhich fzf dash htop lazygit ripgrep thefuck nvim direnv fish mprocs git-delta fnm pgcli httpie hexyl sccache)
+programs=(exa lsd fd bat dust procs tealdeer bottom broot skim tokei sd bandwhich fzf dash htop lazygit ripgrep thefuck neovim direnv fish git-delta pgcli httpie hexyl sccache npm tmux)
 fonts=(font-hack-nerd-font font-agave-nerd-font font-jetbrains-mono-nerd-font)
 
 if [ -x "$(command -v brew)" ]; then
     brew install ${programs[@]}
     brew install ${fonts[@]}
 elif [ -x "$(command -v pacman)" ]; then
-    sudo pacman -S ${programs[@]}
-    sudo pacman -S ${fonts[@]}
+    sudo pacman -S --needed ${programs[@]} 2>/dev/null
+    # sudo pacman -S ${fonts[@]}
 fi
