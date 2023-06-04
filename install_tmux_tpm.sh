29a7a2a8b86d190dd3344dcf73e93531da712bfd
Tue Phan <icyrainz@gmail.com>
2023-06-04 10:41:22 -0700

    update tmux conf

---
+++ install_tmux_tpm.sh
@@ -0,0 +1,2 @@
+[[ -d ~/.config/tmux/plugins/tpm ]] ||
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
--- tmux/install_plugin_manager.zsh
+++
@@ -1,2 +0,0 @@
-[[ -d ~/.tmux/plugins/tpm ]] ||
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
