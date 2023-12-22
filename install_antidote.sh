490cc7ece261bbe7608af067c40920b97d9fd47e
Tue Phan <icyrainz@gmail.com>
2023-12-22 10:06:01 -0800

    tmux-only install

--- install_antidote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-[[ -d ~/.antidote ]] ||
-    git clone https://github.com/mattmc3/antidote ~/.antidote
-
-# Install Antidote
-chmod +x ~/.antidote/antidote
-~/.antidote/antidote update
