95e2d174b0701896a32698520720d03f42a0b113
Tue Phan <icyrainz@gmail.com>
2024-01-07 20:12:49 -0800

    enable qf

--- nvim/lua/plugins/quickfix.lua
+++ nvim/lua/plugins/quickfix.lua
@@ -1,6 +1,5 @@
 return {
   {
-    enabled = false,
     "stevearc/qf_helper.nvim",
     event = "VeryLazy",
     keys = {
@@ -9,12 +8,12 @@ return {
     },
     opts = {
       quickfix = {
-        autoclose = true,         -- Autoclose qf if it's the only open window
-        default_bindings = false, -- Set up recommended bindings in qf window
-        default_options = true,   -- Set recommended buffer and window options
-        max_height = 10,          -- Max qf height when using open() or toggle()
-        min_height = 1,           -- Min qf height when using open() or toggle()
-        track_location = true,    -- Keep qf updated with your current location
+        autoclose = true,        -- Autoclose qf if it's the only open window
+        default_bindings = true, -- Set up recommended bindings in qf window
+        default_options = true,  -- Set recommended buffer and window options
+        max_height = 10,         -- Max qf height when using open() or toggle()
+        min_height = 1,          -- Min qf height when using open() or toggle()
+        track_location = true,   -- Keep qf updated with your current location
       },
     },
   },
