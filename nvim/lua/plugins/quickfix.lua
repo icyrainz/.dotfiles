b8512c3c38e27298d9c89217cce615f8fd82bce9
Tue Phan <icyrainz@gmail.com>
2024-01-14 02:44:11 -0800

    quickfix & telescope

---
+++ nvim/lua/plugins/quickfix.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "stevearc/qf_helper.nvim",
+    ft = "qf",
+    opts = {
+      quickfix = {
+        default_bindings = false,
+      },
+    },
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    dependencies = {
+      "junegunn/fzf",
+      config = function()
+        vim.fn["fzf#install"]()
+      end,
+    },
+    ft = "qf",
+    opts = {
+      auto_resize_height = true,
+      preview = {
+        auto_preview = false,
+      },
+      filter = {
+        fzf = {
+          extra_opts = {
+            description = "Extra options for fzf",
+            default = { "--bind", "ctrl-o:toggle-all,ctrl-f:preview-half-page-down,ctrl-b:preview-half-page-up" },
+          },
+        },
+      },
+    },
+  },
+}
