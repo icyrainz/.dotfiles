51ce4eff234d95b2a7948d9f479e8c4e25db5110
Tue Phan <icyrainz@gmail.com>
2024-01-23 01:34:00 -0800

    add edgy

--- nvim/lua/plugins/dap-ui.lua
+++ nvim/lua/plugins/dap-ui.lua
@@ -1,57 +1,87 @@
 return {
-  "rcarriga/nvim-dap-ui",
-  opts = {
-    layouts = {
-      {
-        elements = {
-          {
-            id = "scopes",
-            size = 0.5,
+  {
+    "rcarriga/nvim-dap-ui",
+    opts = {
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.5,
+            },
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            -- {
+            --   id = "stacks",
+            --   size = 0.25
+            -- } ,
+            {
+              id = "watches",
+              size = 0.25,
+            },
           },
-          {
-            id = "breakpoints",
-            size = 0.25,
-          },
-          -- {
-          --   id = "stacks",
-          --   size = 0.25
-          -- } ,
-          {
-            id = "watches",
-            size = 0.25,
-          },
+          position = "right",
+          size = 10,
         },
-        position = "right",
-        size = 10,
-      },
-      {
-        elements = {
-          {
-            id = "console",
-            size = 0.75,
+        {
+          elements = {
+            {
+              id = "console",
+              size = 0.75,
+            },
+            -- {
+            --   id = "repl",
+            --   size = 0.5
+            -- }
           },
-          -- {
-          --   id = "repl",
-          --   size = 0.5
-          -- }
+          position = "bottom",
+          size = 15,
         },
-        position = "bottom",
-        size = 15,
       },
     },
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      -- dap.listeners.before.event_terminated["dapui_config"] = function()
+      --   dapui.close({})
+      -- end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
   },
-  config = function(_, opts)
-    local dap = require("dap")
-    local dapui = require("dapui")
-    dapui.setup(opts)
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open({})
-    end
-    -- dap.listeners.before.event_terminated["dapui_config"] = function()
-    --   dapui.close({})
-    -- end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close({})
-    end
-  end,
+  {
+    "folke/edgy.nvim",
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = "Scopes",
+        ft = "dapui_scopes",
+        pinned = false,
+      })
+      table.insert(opts.right, {
+        title = "Breakpoints",
+        ft = "dapui_breakpoints",
+        pinned = false,
+      })
+      table.insert(opts.right, {
+        title = "Watches",
+        ft = "dapui_watches",
+        pinned = false,
+      })
+
+      opts.bottom = opts.bottom or {}
+      table.insert(opts.bottom, {
+        title = "Console",
+        ft = "dapui_console",
+        pinned = false,
+      })
+    end,
+  },
 }
--- nvim/lua/plugins/outline.lua
+++ nvim/lua/plugins/outline.lua
@@ -1,7 +1,21 @@
 return {
-  "hedyhli/outline.nvim",
-  keys = {
-    { "<leader>co", "<CMD>Outline<CR>", desc = "Symbols outline" },
+  {
+    "hedyhli/outline.nvim",
+    keys = {
+      { "<leader>co", "<CMD>Outline<CR>", desc = "Symbols outline" },
+    },
+    opts = {},
   },
-  opts = {},
+  {
+    "folke/edgy.nvim",
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = "Symbols Outline",
+        ft = "Outline",
+        pinned = true,
+        open = "Outline",
+      })
+    end,
+  },
 }
