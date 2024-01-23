d827b32cc5b2b8caa71b8edcbdd8e1cfa484eb64
Tue Phan <icyrainz@gmail.com>
2024-01-23 00:23:19 -0800

    add mode

---
+++ nvim/lua/plugins/modes.lua
@@ -0,0 +1,14 @@
+return {
+  "mvllow/modes.nvim",
+  event = "VeryLazy",
+  opts = {
+    colors = {
+      copy = "#ffc777",
+      delete = "#ff757f",
+      insert = "#c3e88d",
+      visual = "#c099ff",
+    },
+    line_opacity = 0.3,
+    set_number = false,
+  },
+}
