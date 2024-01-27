f479ef0b28e48813c928e7fae28d7c5069692f1e
Tue Phan <tue.phan@humaninterest.com>
2024-01-26 16:10:01 -0800

    fisher nvm

--- install_tools.sh
+++ install_tools.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 programs=(lsd fd bat dust procs tealdeer bottom broot skim tokei sd bandwhich fzf dash htop lazygit ripgrep thefuck neovim direnv fish git-delta pgcli httpie hexyl sccache npm tmux openvpn nmap terminal-notifier coreutils curl git kondo nushell)
-programs_brew_only=(mprocs fnm)
-fonts=(font-jetbrains-mono-nerd-font)
+programs_brew_only=(mprocs)
+fonts=(font-jetbrains-mono-nerd-font font-iosevka-nerd-font)
 
 if [ -x "$(command -v brew)" ]; then
-    brew install ${programs[@]}
-    brew install ${programs_brew_only[@]}
-    brew install ${fonts[@]}
+	brew install ${programs[@]}
+	brew install ${programs_brew_only[@]}
+	brew install ${fonts[@]}
 elif [ -x "$(command -v pacman)" ]; then
-    sudo pacman -S --needed ${programs[@]} 2>/dev/null
+	sudo pacman -S --needed ${programs[@]} 2>/dev/null
 fi
