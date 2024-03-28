8fdcb3085a9f682d0f9c6ac72abd6bc03ac23f55
Tue Phan <tue.phan@humaninterest.com>
2024-03-27 20:30:55 -0700

    update nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,13 +11,6 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        config = function()
-          require("telescope").load_extension("fzf")
-        end,
-      },
-      {
         "nvim-telescope/telescope-live-grep-args.nvim",
         config = function()
           local telescope = require("telescope")
