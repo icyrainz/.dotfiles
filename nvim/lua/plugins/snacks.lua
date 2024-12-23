818b15c5d9b4f9d7fe581685af2ff2fa3b5c17e7
Tue Phan <icyrainz@gmail.com>
2024-12-23 01:16:25 -0800

    update snack

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,10 +4,17 @@ return {
     opts = {
       dashboard = {
         sections = {
-          { section = "header" },
+          { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            icon = " ",
+            title = "Recent Files",
+            section = "recent_files",
+            limit = 8,
+            cwd = true,
+            indent = 2,
+            padding = 1,
+          },
           { section = "startup" },
         },
       },
